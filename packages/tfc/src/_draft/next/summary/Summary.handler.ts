3753a16dd3302d0d977e080b314df5ae557fb3dd
fgarcia <253157+fgarcia@users.noreply.github.com>
2025-07-29 07:07:01 +0200

    wip

--- packages/tfc/src/_draft/next/summary/Summary.handler.ts
+++ packages/tfc/src/_draft/next/summary/Summary.handler.ts
@@ -169,329 +169,304 @@ export class SummaryHandler {
 
     log.put().put(`${today_str} : Week ${weekNumberNow} {theme name?}`).put()
 
-    let byNearTimeGroups = Object.groupBy(data.calendar, item => {
-      if (isThisWeek(item.date)) return 'week'
-      if (
-        isWithinInterval(item.date, {
-          start: addDays(now, 7),
-          end: addDays(now, 30),
+    const printWaiting = (all: PathItem[]) => {
+      printSection('Waiting')
+      log.indent()
+
+      let rows = all
+        .map(x => {
+          //let started = x.after ? x.after.toISOString().slice(0, 10) : ''
+          let started = x.after_v2.date
+            ? timeDiff({ date: x.after_v2.date })
+            : ''
+          let item = {
+            // path: log.link({ text: x.path, path: x.pathFull }),
+            path: toPathPrint(x),
+            started,
+            // due: '',
+          }
+
+          if (now < x.after_v2.date) {
+            let days = differenceInCalendarDays(x.after_v2.date, now)
+            if (days > printOptions.hideAfterDays) {
+              return null
+            }
+
+            dimKeysApply(item)
+          }
+          return item
         })
-      )
-        return 'month'
-      if (
-        isWithinInterval(item.date, {
-          start: addDays(now, 30),
-          end: addDays(now, 360),
-        })
-      )
-        return 'year'
+        .filter(Boolean)
 
-      return 'rest'
-    })
-
-    for (let [key, val] of Object.entries(data)) {
-      // TODO wtf? clean #type
-      type foo = keyof typeof data
-      switch (key as foo) {
-        case 'waiting': {
-          printSection('Waiting')
-          log.indent()
-
-          let all = val as PathItem[]
-
-          let rows = all
-            .map(x => {
-              //let started = x.after ? x.after.toISOString().slice(0, 10) : ''
-              let started = x.after_v2.date
-                ? timeDiff({ date: x.after_v2.date })
-                : ''
-              let item = {
-                // path: log.link({ text: x.path, path: x.pathFull }),
-                path: toPathPrint(x),
-                started,
-                // due: '',
-              }
-
-              if (now < x.after_v2.date) {
-                let days = differenceInCalendarDays(x.after_v2.date, now)
-                if (days > printOptions.hideAfterDays) {
-                  return null
-                }
-
-                dimKeysApply(item)
-              }
-              return item
-            })
-            .filter(Boolean)
-
-          printTable({
-            rows,
-            log,
-            config: { path: { padding: PathPadding, head: '' } },
-          })
-          log.dedent()
-          break
-        }
-        case 'calendar': {
-          const printCalendar = () => {
-            printSection('Calendar')
-            log.indent()
-            const putLine = (
-              item: { title; item; date },
-              //CalendarItem
-            ) => {
-              let date = item.date.toISOString().slice(0, 10)
-              let link = NodeLogger.link({
-                text: item.title,
-                path: item.item.pathFull,
-              })
-              log.put(date, link)
-            }
-
-            let printAll = (val: { date; title; item }[]) => {
-              let all = val.sort(
-                (lhs, rhs) => lhs.date.getTime() - rhs.date.getTime(),
-              )
-              for (let item of all) {
-                putLine(item)
-              }
-            }
-
-            log = log.put('In a week').indent()
-            if (byNearTimeGroups.week) {
-              log.put('..todo')
-            } else {
-              log.put(log.style.dim('none'))
-            }
-            log = log.dedent()
-
-            log = log.put('In a month').indent()
-            if (byNearTimeGroups.month) {
-              printAll(byNearTimeGroups.month)
-            } else {
-              log.put('..none')
-            }
-            log = log.dedent()
-
-            log = log.put('In a year').indent()
-            if (byNearTimeGroups.year) {
-              printAll(byNearTimeGroups.year)
-            } else {
-              log.put('..none')
-            }
-            log = log.dedent()
-          }
-          printCalendar()
-          break
-        }
-        case 'now': {
-          const printNow = () => {
-            printSection('NOW')
-            log.indent()
-            // LOG group by inner dir
-            // console.log(a1)
-            let all = val as PathItem[]
-
-            type Row = { path; modified; due; after }
-            let rows: Row[] = []
-            all = all.sort(
-              (lhs, rhs) => rhs.mtime.getTime() - lhs.mtime.getTime(),
-            )
-            all.forEach(x => {
-              // if (x.show.startsWith('_')) return
-              let time = x.mtime.toISOString().slice(0, 10)
-              // let time = ''
-
-              let path = toPathPrint(x)
-              let next = {
-                path,
-                modified: timeDiff({ date: x.mtime, color: false }),
-                due: '',
-                after: x.after ? x.after.toISOString().slice(0, 10) : '',
-              }
-
-              if (x.after) {
-                if (x.after < now) {
-                  rows.push(next)
-                }
-              } else {
-                rows.push(next)
-              }
-            })
-
-            printTable({
-              rows,
-              log,
-              config: {
-                path: { padding: PathPadding, head: '' },
-                modified: { padding: 12, head: 'Modified *' },
-              },
-            })
-
-            log.dedent()
-          }
-          printNow()
-          break
-        }
-        // case 'review':
-        //   // TODO
-        //   printSection('Review')
-        //   log.indent().dev('Todo review', key)
-        //   log.dedent()
-        //   break
-
-        case 'active': {
-          printSection('Active')
-
-          let all = val as PathItem[]
-          let t1 = Object.groupBy(all, x => {
-            if (!x.after) return 'others'
-            if (now.getTime() < x.after.getTime()) {
-              return 'postponed'
-            }
-            return 'started'
-          })
-
-          log.indent()
-          log.put(
-            `total=${val.length} active=${t1.started?.length ?? 0} postponed=${
-              t1.postponed?.length ?? 0
-            }`,
-          )
-
-          let rows: Fox[] = []
-
-          all.forEach(pathItem => {
-            // let mtime = x.mtime.toISOString().slice(0, 10)
-            let started = ''
-            if (pathItem.after_v2?.date) {
-              started = timeDiff({ date: pathItem.after_v2.date, color: false })
-            } else {
-              started = pathItem.after_v2?.value ?? ''
-              if (pathItem.after_v2?.type === 'reference') {
-                // TODO :multi-index
-                let found = this.index?.findByReference(pathItem.after_v2.value)
-                if (found) {
-                  started = NodeLogger.link({
-                    text: started,
-                    path: found.pathFull,
-                  })
-                }
-              } else if (pathItem.after_v2?.type === 'relative') {
-                started = NodeLogger.style.yellow(`rel(${started})`)
-              }
-            }
-
-            // --
-            let isActive = true
-            if (pathItem.after_v2) {
-              isActive = pathItem.after_v2.date
-                ? now.getTime() > pathItem.after.getTime()
-                : false
-
-              if (pathItem.after_v2.type === 'reference') {
-                if (this.index) {
-                  let found = this.index.findByReference(
-                    pathItem.after_v2.value,
-                  )
-                  if (found) {
-                    isActive = found.done ?? false
-                  }
-                } else {
-                  log.dev('todo multi index')
-                }
-              }
-            }
-
-            let due = pathItem.before ? timeDiff({ date: pathItem.before }) : ''
-
-            let row = { path: toPathPrint(pathItem), started, due }
-
-            pathItem.sections
-              .filter(x => x.type === 'todo')
-              .forEach(section => {
-                // log.dev(section)
-                let link = NodeLogger.link({
-                  text: 'sec',
-                  path: pathItem.pathFull,
-                  lineNumber: section.lineNumber,
-                  template: 'vscode',
-                })
-                if (section.lineNumber) {
-                  // TODO review #hack #dirty
-                  // ... just want to see title when hovering link in terminal
-                  // ?? just add queryParams to link utility?
-                  {
-                    // link = link.replace(
-                    //   `:${section.lineNumber}`,
-                    //   `:${section.lineNumber}?title=${section.title.replace(
-                    //     / /g,
-                    //     '_',
-                    //   )}`,
-                    // )
-                  }
-                }
-                row.path += ` ${link}`
-              })
-
-            if (!isActive) {
-              let days = differenceInCalendarDays(pathItem.after_v2.date, now)
-              if (days > printOptions.hideAfterDays) {
-                return null
-              }
-
-              dimKeysApply(row)
-            }
-
-            rows.push(row)
-          })
-
-          printTable({
-            rows,
-            log,
-            config: {
-              path: { padding: PathPadding, head: '' },
-              started: { padding: 12 },
-            },
-          })
-          //
-          log.dedent()
-
-          break
-        }
-        case 'focus': {
-          const printFocus = () => {
-            printSection('Focus')
-            // let rows: Fox[] = []
-            let rows: { path }[] = []
-
-            let all = val as PathItem[]
-            for (let pItem of all) {
-              let weekFocus = getWeek(pItem.focus.date)
-              let weekNow = getWeek(now)
-              let row = { path: toPathPrint(pItem), week: weekFocus }
-              if (weekFocus !== weekNow) {
-                dimKeysApply(row)
-              }
-              rows.push(row)
-            }
-            printTable({
-              rows,
-              log,
-              config: {
-                path: { padding: PathPadding, head: '' },
-                // started: { padding: 12 },
-              },
-            })
-          }
-          printFocus()
-          break
-        }
-
-        default:
-          throw Error(`unknown key ${key}`)
-      }
+      printTable({
+        rows,
+        log,
+        config: { path: { padding: PathPadding, head: '' } },
+      })
+      log.dedent()
     }
+
+    const printCalendar = () => {
+      printSection('Calendar')
+      log.indent()
+
+      let byNearTimeGroups = Object.groupBy(data.calendar, item => {
+        if (isThisWeek(item.date)) return 'week'
+        if (
+          isWithinInterval(item.date, {
+            start: addDays(now, 7),
+            end: addDays(now, 30),
+          })
+        )
+          return 'month'
+        if (
+          isWithinInterval(item.date, {
+            start: addDays(now, 30),
+            end: addDays(now, 360),
+          })
+        )
+          return 'year'
+
+        return 'rest'
+      })
+
+      const putLine = (
+        item: { title; item; date },
+        //CalendarItem
+      ) => {
+        let date = item.date.toISOString().slice(0, 10)
+        let link = NodeLogger.link({
+          text: item.title,
+          path: item.item.pathFull,
+        })
+        log.put(date, link)
+      }
+
+      let printAll = (val: { date; title; item }[]) => {
+        let all = val.sort(
+          (lhs, rhs) => lhs.date.getTime() - rhs.date.getTime(),
+        )
+        for (let item of all) {
+          putLine(item)
+        }
+      }
+
+      log = log.put('In a week').indent()
+      if (byNearTimeGroups.week) {
+        log.put('..todo')
+      } else {
+        log.put(log.style.dim('none'))
+      }
+      log = log.dedent()
+
+      log = log.put('In a month').indent()
+      if (byNearTimeGroups.month) {
+        printAll(byNearTimeGroups.month)
+      } else {
+        log.put('..none')
+      }
+      log = log.dedent()
+
+      log = log.put('In a year').indent()
+      if (byNearTimeGroups.year) {
+        printAll(byNearTimeGroups.year)
+      } else {
+        log.put('..none')
+      }
+      log = log.dedent()
+    }
+
+    const printNow = (all: PathItem[]) => {
+      printSection('NOW')
+      log.indent()
+      // LOG group by inner dir
+      // console.log(a1)
+
+      type Row = { path; modified; due; after }
+      let rows: Row[] = []
+      all = all.sort((lhs, rhs) => rhs.mtime.getTime() - lhs.mtime.getTime())
+      all.forEach(x => {
+        // if (x.show.startsWith('_')) return
+        let time = x.mtime.toISOString().slice(0, 10)
+        // let time = ''
+
+        let path = toPathPrint(x)
+        let next = {
+          path,
+          modified: timeDiff({ date: x.mtime, color: false }),
+          due: '',
+          after: x.after ? x.after.toISOString().slice(0, 10) : '',
+        }
+
+        if (x.after) {
+          if (x.after < now) {
+            rows.push(next)
+          }
+        } else {
+          rows.push(next)
+        }
+      })
+
+      printTable({
+        rows,
+        log,
+        config: {
+          path: { padding: PathPadding, head: '' },
+          modified: { padding: 12, head: 'Modified *' },
+        },
+      })
+
+      log.dedent()
+    }
+
+    const printFocus = (all: PathItem[]) => {
+      printSection('Focus')
+      // let rows: Fox[] = []
+      let rows: { path }[] = []
+
+      for (let pItem of all) {
+        let weekFocus = getWeek(pItem.focus.date)
+        let weekNow = getWeek(now)
+        let row = { path: toPathPrint(pItem), week: weekFocus }
+        if (weekFocus !== weekNow) {
+          dimKeysApply(row)
+        }
+        rows.push(row)
+      }
+      printTable({
+        rows,
+        log,
+        config: {
+          path: { padding: PathPadding, head: '' },
+          // started: { padding: 12 },
+        },
+      })
+    }
+
+    const printActive = (val: PathItem[]) => {
+      printSection('Active')
+
+      let all = val as PathItem[]
+      let t1 = Object.groupBy(all, x => {
+        if (!x.after) return 'others'
+        if (now.getTime() < x.after.getTime()) {
+          return 'postponed'
+        }
+        return 'started'
+      })
+
+      log.indent()
+      log.put(
+        `total=${val.length} active=${t1.started?.length ?? 0} postponed=${
+          t1.postponed?.length ?? 0
+        }`,
+      )
+
+      let rows: Fox[] = []
+
+      all.forEach(pathItem => {
+        // let mtime = x.mtime.toISOString().slice(0, 10)
+        let started = ''
+        if (pathItem.after_v2?.date) {
+          started = timeDiff({
+            date: pathItem.after_v2.date,
+            color: false,
+          })
+        } else {
+          started = pathItem.after_v2?.value ?? ''
+          if (pathItem.after_v2?.type === 'reference') {
+            // TODO :multi-index
+            let found = this.index?.findByReference(pathItem.after_v2.value)
+            if (found) {
+              started = NodeLogger.link({
+                text: started,
+                path: found.pathFull,
+              })
+            }
+          } else if (pathItem.after_v2?.type === 'relative') {
+            started = NodeLogger.style.yellow(`rel(${started})`)
+          }
+        }
+
+        // --
+        let isActive = true
+        if (pathItem.after_v2) {
+          isActive = pathItem.after_v2.date
+            ? now.getTime() > pathItem.after.getTime()
+            : false
+
+          if (pathItem.after_v2.type === 'reference') {
+            if (this.index) {
+              let found = this.index.findByReference(pathItem.after_v2.value)
+              if (found) {
+                isActive = found.done ?? false
+              }
+            } else {
+              log.dev('todo multi index')
+            }
+          }
+        }
+
+        let due = pathItem.before ? timeDiff({ date: pathItem.before }) : ''
+
+        let row = { path: toPathPrint(pathItem), started, due }
+
+        pathItem.sections
+          .filter(x => x.type === 'todo')
+          .forEach(section => {
+            // log.dev(section)
+            let link = NodeLogger.link({
+              text: 'sec',
+              path: pathItem.pathFull,
+              lineNumber: section.lineNumber,
+              template: 'vscode',
+            })
+            if (section.lineNumber) {
+              // TODO review #hack #dirty
+              // ... just want to see title when hovering link in terminal
+              // ?? just add queryParams to link utility?
+              {
+                // link = link.replace(
+                //   `:${section.lineNumber}`,
+                //   `:${section.lineNumber}?title=${section.title.replace(
+                //     / /g,
+                //     '_',
+                //   )}`,
+                // )
+              }
+            }
+            row.path += ` ${link}`
+          })
+
+        if (!isActive) {
+          let days = differenceInCalendarDays(pathItem.after_v2.date, now)
+          if (days > printOptions.hideAfterDays) {
+            return null
+          }
+
+          dimKeysApply(row)
+        }
+
+        rows.push(row)
+      })
+
+      printTable({
+        rows,
+        log,
+        config: {
+          path: { padding: PathPadding, head: '' },
+          started: { padding: 12 },
+        },
+      })
+      //
+      log.dedent()
+    }
+
+    printWaiting(data.waiting)
+    printCalendar()
+    printNow(data.now)
+    printActive(data.active)
+    printFocus(data.focus)
 
     let indexLabel = NodeLogger.link({
       text: 'index',
